a204e764b10356fa8bff18aa45797f3275ddd2b9
Joshua <josuealejandrof926@gmail.com>
2025-07-13 01:07:14 +0200

    Make getWeatherData feat

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 // * First thing we do is get the input from the DOM
-console.log("Se actualiza?")
-
 const $input = document.querySelector("input");
 
-
-
 $input.addEventListener("keydown", (e) => {
-
-
     if (e.key === "Enter") {
-        console.log("Ahora esto se ve")
-        getCoordinates("Madrid")
+       getWeatherData($input.value);
     }
 })
 
-async function getCoordinates(cityName) {
-    const response = await fetch("https://geocoding-api.open-meteo.com/v1/search?name=Madrid&count=1")
-    const data = await response.json()
-    console.log(data)
-}
 
+async function getWeatherData(cityName) {
+
+    const city = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=1`
+    ).then(async (res) => await res.json()).then(
+        data => data.results[0]
+    );
+
+    console.log(city)
+
+    const response =
+      await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${city.latitude}&longitude=${city.longitude}&current=temperature_2m,weathercode,apparent_temperature&hourly=temperature_2m,apparent_temperature,precipitation_probability,weathercode,uv_index,windspeed_10m&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max,weathercode&timezone=auto
+`);
+    
+    const data = await response.json()
+
+    console.log(data)
+
+}
